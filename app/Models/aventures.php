af7b645e114bbced8acc150cd22ce7847030a90b
salah <akilsalaheddine203@gmail.com>
2024-01-31 23:10:19 +0100

    tables done

---
+++ app/Models/aventures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class aventures extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_01_31_214929_create_aventures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aventures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_users')->constrained('users');
+            $table->string('ville');
+            $table->string('paye');
+            $table->string('continent');
+            $table->string('image');
+            $table->string('description');
+            $table->string('conseils');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aventures');
+    }
+};
