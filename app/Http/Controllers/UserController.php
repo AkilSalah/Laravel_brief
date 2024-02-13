c7dabb13f2d1420fe0d8dc199b8740d749d6ac08
salah <akilsalaheddine203@gmail.com>
2024-02-13 10:26:45 +0100

    no commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
         $user = User::create($validation);
         auth()->login($user);
         
-        return redirect('/utilisateur') ;
+        return redirect('/utilisateur');
 
     }
     public function logout(){
